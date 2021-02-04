63e21927d42563983391e4a7d2d4bb5dbe347785
maximegourgues <maxime.gourgues@etud.univ-jfc.fr>
2021-02-04 22:03:30 +0100

    Update main.c
    Init main

---
+++ main.c
@@ -0,0 +1,60 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h> 
+
+#define BOARD_SIZE 2    //N
+#define DECK_SIZE 25    //P
+#define TILE_SIDES 4    //C
+#define MAX_PLAYERS 5
+
+struct file_t {
+    int head;
+    int queue;
+    tile_t* suivant;
+}
+
+enum color_t { BLUE, RED , GREEN, YELLOW, CYAN, PURPLE, PINK, BLACK, GREY, WHITE };
+
+struct player {
+    const char *name;
+    int tiles_count;
+    struct tile draw;
+};
+
+
+struct tile_t {
+    enum color colors[TILE_SIDES];
+};
+
+struct deck_t {
+    int size;
+    struct tile tiles[DECK_SIZE];
+}
+
+struct deck2_t {
+    int size;
+    struct file_t tiles;
+}
+struct position_t {
+    int x; 
+    int y; 
+};
+
+struct cell_t {
+    struct tile* tile;    
+};
+
+sruct board_t {
+    int range = 2*BOARD_SIZE+1;
+    struct cell cells[range][range];
+    int size; 
+};
+
+struct game {
+    const int player_count;
+    struct player players[MAX_PLAYERS];
+    int score;
+   
+};
