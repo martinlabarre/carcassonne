16cfd4cd37ccb64704da14319dfebd0ed0c48876
LABARRE Martin <mlabarre@amanominaka.pedago.ipb.fr>
2021-02-10 11:23:37 +0100

    fix syntaxe

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-/* 
-Les pavages de Carcassonne - Projet Algorithmique et Programmation C 
-ENSEIRB-MATMECA - RSI S6 
+/*
+Les pavages de Carcassonne - Projet Algorithmique et Programmation C
+ENSEIRB-MATMECA - RSI S6
 Maxime GOURGUES, Martin LABARRE
 */
 
@@ -8,59 +8,62 @@ Maxime GOURGUES, Martin LABARRE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h> 
+#include <stdbool.h>
 
-#define BOARD_SIZE 2    //N
+#define N 2 //N
+#define BOARD_SIZE (2*N+1) //Dimension du plateau
 #define DECK_SIZE 25    //P
 #define TILE_SIDES 4    //C
 #define MAX_PLAYERS 5
 
+typedef enum { BLUE, RED , GREEN, YELLOW, CYAN, PURPLE, PINK, BLACK, GREY } color_t ;
+
+struct tile_t {
+    color_t colors[TILE_SIDES];
+};
+
+typedef struct tile_t Tile;
+
 struct file_t {
     int head;
     int queue;
-    tile_t* suivant;
-}
-
-typedef enum color_t { BLUE, RED , GREEN, YELLOW, CYAN, PURPLE, PINK, BLACK, GREY, WHITE };
+    Tile* suivant;
+};
 
 struct player {
     const char *name;
     int tiles_count;
-    struct tile draw;
-};
-
-struct tile_t {
-    enum color colors[TILE_SIDES];
+    struct tile_t draw;
 };
 
 struct deck_t {
     int size;
-    struct tile tiles[DECK_SIZE];
-}
+    struct tile_t tiles[DECK_SIZE];
+};
 
 struct deck2_t {
     int size;
     struct file_t tiles;
-}
+};
+
 struct position_t {
-    int x; 
-    int y; 
+    int x;
+    int y;
 };
 
 struct cell_t {
-    struct tile* tile;    
+    struct tile_t* tile;
 };
 
 struct board_t {
-    int range = 2*BOARD_SIZE+1;
-    struct cell cells[range][range];
-    int size; 
+    struct cell_t cells[BOARD_SIZE][BOARD_SIZE];
+    int size;
 };
 
 struct game {
     const int player_count;
     struct player players[MAX_PLAYERS];
-    int score; 
+    int score;
 };
 
 int main()
